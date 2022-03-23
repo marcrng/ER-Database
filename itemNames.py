cdca3766704652b34c0c4ca8f7c9cec437b4ad3a
marcrng <59784362+marcrng@users.noreply.github.com>
2022-03-23 16:55:01 -0700

    Scrape names and categories from webpage into .csv

--- itemNames.py
+++ itemNames.py
@@ -1,20 +1,25 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
 
 URL = "https://eip.gg/elden-ring/search/?wpv_aux_current_post_id=85703&wpv_aux_parent_post_id=85703&wpv_view_count=85707"
 page = requests.get(URL)
+print(page.status_code)
 
 soup = BeautifulSoup(page.content, "html.parser")
 # Find first item name
 first_name = soup.findAll('a')[39].text
 names = soup.findAll('a')
 
+initial = []
+
 # Build iteration to extract all item names from first page
 for txt in names[39:64]:
+    initial.append(txt.getText())
     print(txt.getText())
 
 # Expand iteration to include all item pages (2-66)
-for i in range(2, 66):
+for i in range(2, 67):
     URL = "https://eip.gg/elden-ring/search/?wpv_aux_current_post_id=85703&wpv_aux_parent_post_id=85703&wpv_view_count=85707"
     pagenum = "&wpv_paged=" + str(i)
     page = requests.get(URL + pagenum)
@@ -24,4 +29,12 @@ for i in range(2, 66):
     first_name = soup.findAll('a')[39].text
     names = soup.findAll('a')
     for txt in names[39:64]:
+        initial.append(txt.getText())
         print(txt.getText())
+
+dfName = pd.DataFrame({
+    "item_name": initial
+})
+
+# Export dataframe to csv
+dfName.to_csv('initialNames.csv')
--- itemNamesCategory.py
+++ itemNamesCategory.py
@@ -1,20 +1,26 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
 
 URL = "https://eip.gg/elden-ring/search/?wpv_aux_current_post_id=85703&wpv_aux_parent_post_id=85703&wpv_view_count=85707"
 page = requests.get(URL)
+print(page.status_code)
 
 soup = BeautifulSoup(page.content, "html.parser")
-# Find first item name
-first_name = soup.findAll('a')[39].text
-names = soup.findAll('a')
 
-# Build iteration to extract all item names from first page
-for txt in names[39:64]:
-    print(txt.getText())
+# Create dict for names, category
+name = []
+category = []
 
-# Expand iteration to include all item pages (2-66)
-for i in range(2, 66):
+# Create dict for name and category for page 1
+for i in soup.findAll('tr')[1:]:
+    tds = i.find_all('td')
+
+    name.append(tds[0].text)
+    category.append(tds[1].text)
+
+# Create dict, name for other pages
+for i in range(2, 67):
     URL = "https://eip.gg/elden-ring/search/?wpv_aux_current_post_id=85703&wpv_aux_parent_post_id=85703&wpv_view_count=85707"
     pagenum = "&wpv_paged=" + str(i)
     page = requests.get(URL + pagenum)
@@ -23,5 +29,15 @@ for i in range(2, 66):
     # Find first item name
     first_name = soup.findAll('a')[39].text
     names = soup.findAll('a')
-    for txt in names[39:64]:
-        print(txt.getText())
+    for i in soup.findAll('tr')[1:]:
+        tds = i.find_all('td')
+
+        name.append(tds[0].text)
+        category.append(tds[1].text)
+
+df = pd.DataFrame({
+    "itemName": name,
+    "itemType": category
+})
+
+df.to_csv('namesType.csv')
